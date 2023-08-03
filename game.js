5d7442a1255939029f37e55965c59697ce51aa2e
Ismail Silat <ismailsilat7@gmail.com>
2023-08-03 22:45:58 +0500

    Add game function

--- game.js
+++ game.js
@@ -42,4 +42,70 @@ function playRound (playerSelection, computerSelection) {
   return num;
 }
 
+function game() {
+  let playerWins = 0;
+  let draws = 0;
+  let computerWins = 0;
+  let playerSelection = "ball";
+  let computerSelection;
+  let outstring;
+  let num;
+  let playerChoice;
+  let computerChoice;
+  let numRound;
+  for (let round = 0; round < 5; round++) {
+    numRound = `Round Number ${round + 1}`;
+    console.log(numRound);
 
+    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissor") {
+      playerSelection = prompt("Please enter your choice").toLowerCase();
+    }
+
+    computerSelection = getComputerChoice().toLowerCase();
+
+    playerChoice = `Your Choice : ${playerSelection}`;
+    console.log(playerChoice);
+
+    computerChoice = `Computer's Choice : ${computerSelection}`;
+    console.log(computerChoice);
+
+    num = playRound(playerSelection, computerSelection);
+
+    switch (num) {
+      case 1:
+        outstring = `You Win! ${playerSelection} beats ${computerSelection} :)`;
+        playerWins++;
+        break;
+      case -1:
+        outstring = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerWins++;
+        break;
+      default:
+        outstring = "It's a Draw";
+        draws++;
+    }
+
+    playerSelection = "ball";
+    console.log(outstring);
+  }
+
+  console.log ("GAME OVER!")
+  let score;
+  score = `Your Score : ${playerWins}`;
+  console.log(score);
+  score = `Computer's Score : ${computerWins}`;
+  console.log(score);
+  score = `Draws : ${draws}`;
+  console.log(score);
+  let result;
+  if (playerWins > computerWins) {
+    result = "Congrats! You Win"
+  } else if (playerWins < computerWins) {
+    result = "You Lose"
+  } else {
+    result = "This game is a draw"
+  }
+  console.log(result);
+}
+
+game();
