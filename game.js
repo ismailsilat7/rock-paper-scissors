6093e205864bde21c2d6509dc627ae73797fa295
Ismail Silat <ismailsilat7@gmail.com>
2023-08-19 20:15:32 +0500

    Organize file & add comments where required

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-
-//Query Selectors as global variables
+// DOM Element Selection
 const content = document.querySelector('.compChoice');
 const roundNum = document.querySelector('.round');
 const result = document.querySelector('.result');
@@ -12,8 +11,12 @@ const gameOutcome = document.querySelector('.outcome');
 const playerChoice = document.querySelector('.player');
 const compChoice = document.querySelector('.comp');
 const selection = document.querySelector('.selection');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissor = document.querySelector('.scissor');
+const reset = document.querySelector('.restart');
 
-//Global Variables 
+// Global Variables
 let computerSelection;
 let playerSelection;
 let playerWins = 0;
@@ -23,99 +26,79 @@ let outstring;
 let num;
 let round = 1;
 
-//Event Listeners to check if a button is clicked
-const rock = document.querySelector('.rock');
+// Event Listeners
 rock.addEventListener('click', () => {
   playerSelection = 'rock';
   play(playerSelection);
 });
 
-const paper = document.querySelector('.paper');
 paper.addEventListener('click', () => {
   playerSelection = 'paper';
   play(playerSelection);
 });
 
-const scissor = document.querySelector('.scissor');
 scissor.addEventListener('click', () => {
   playerSelection = 'scissor';
   play(playerSelection);
 });
 
-const reset = document.querySelector('.restart');
 reset.addEventListener('click', () => {
   restart();
-})
+});
 
-//calls eachRound if condition is met
+selection.addEventListener('click', () => {
+  if (round <= 5) {
+    const audio = document.querySelector('.tink');
+    audio.currentTime = 0;
+    audio.play();
+  }
+});
+
+// Function to play the game
 function play(playerSelection) {
   if (round <= 5) {
     eachRound(playerSelection);
     playerChoice.src = sourcestr(playerSelection);
     playerChoice.alt = playerSelection;
   }
-};
-
-// To get computer's choice
-function getComputerChoice () {
-  //generates a rand num between 1 & 3
-  let num = Math.floor(Math.random() * (3)) + 1;
-  let outstring;
-  if (num === 1) {
-    outstring = "Rock";
-  } else if (num === 2) {
-    outstring = "Paper";
-  }
-  else {
-    outstring = "Scissor";
-  }
-  return outstring;
 }
 
+// Function to get computer's choice
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissor'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
 
-// to get result of each round
-function playRound (playerSelection, computerSelection) {
-  let num;
-  // num = 0 is tie, num = 1 indicates that player won, num = -1 indicates that computer won.
+// Function to determine the result of each round
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    num = 0;
-  } else if (playerSelection === "rock") {
-    if (computerSelection === "paper"){
-        num = -1;
-    } else {
-      num = 1;
-    }
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "scissor") {
-      num = -1;
-    } else {
-      num = 1;
-    }
+    return 0; // It's a draw
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissor') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissor' && computerSelection === 'paper')
+  ) {
+    return 1; // Player wins
   } else {
-    if (computerSelection === "rock") {
-      num = -1;
-    } else {
-      num = 1;
-    }
+    return -1; // Computer wins
   }
-  return num;
 }
 
-
-//plays each round and updates the global variable
+// Function to play each round and update game state
 function eachRound(playerSelection) {
-  //get computer's choice
   computerSelection = getComputerChoice();
-  if (round == 5) {
-    roundNum.textContent = `Last Round`;
+
+  if (round === 5) {
+    roundNum.textContent = 'Last Round';
   } else {
     roundNum.textContent = `Round ${round}`;
   }
 
-  compChoice.alt = computerSelection
+  compChoice.alt = computerSelection;
   computerSelection = computerSelection.toLowerCase();
-  compChoice.src = sourcestr(computerSelection)
-  //getting result of round & getting output
+  compChoice.src = sourcestr(computerSelection);
+
   num = playRound(playerSelection, computerSelection);
   switch (num) {
     case 1:
@@ -133,47 +116,49 @@ function eachRound(playerSelection) {
       draws++;
       drawCount.textContent = `Draws : ${draws}`;
   }
+
   roundEnd.textContent = outstring;
 
   if (round >= 5) {
     let outcome;
     let audio;
     if (playerWins > computerWins) {
-      outcome = "Congrats! You Win";
+      outcome = 'Congrats! You Win';
       audio = document.querySelector('.game-win');
     } else if (playerWins < computerWins) {
-      outcome = "You Lose"
+      outcome = 'You Lose';
       audio = document.querySelector('.you-lose');
     } else {
-      outcome = "This game is a draw";
-      audio = document.querySelector('.game-over')
+      outcome = 'This game is a draw';
+      audio = document.querySelector('.game-over');
     }
     gameOutcome.textContent = outcome;
     audio.currentTime = 0;
     audio.play();
   }
-  round++;
-  console.log(round);
-};
 
-//to change source of image
-function sourcestr(selection) {
-  let outstring;
-  if (selection == 'rock') {
-    outstring = './img/rock-white.png';
-  } else if (selection == 'paper') {
-    outstring = './img/paper-white.png';
-  } else if (selection == 'scissor') {
-    outstring = './img/scissor-white.png';
-  } else if (selection == 'user') {
-    outstring = './img/user.png';
-  } else if (selection == 'comp') {
-    outstring = './img/comp.png';
-  }
-  return outstring;
+  round++;
 }
 
-//to reset all values when default
+// Function to determine image source
+function sourcestr(selection) {
+  switch (selection) {
+    case 'rock':
+      return './img/rock-white.png';
+    case 'paper':
+      return './img/paper-white.png';
+    case 'scissor':
+      return './img/scissor-white.png';
+    case 'user':
+      return './img/user.png';
+    case 'comp':
+      return './img/comp.png';
+    default:
+      return '';
+  }
+}
+
+// Function to reset the game
 function restart() {
   roundNum.textContent = "Choose your weapon to get started";
   roundEnd.textContent = "";
@@ -188,14 +173,6 @@ function restart() {
   playerChoice.src = sourcestr('user');
   playerChoice.alt = 'user';
   compChoice.src = sourcestr('comp');
-  playerChoice.alt = 'comp'
+  compChoice.alt = 'comp';
 }
-
-selection.addEventListener('click', () => {
-  if (round <= 5) {
-  const audio = document.querySelector('.tink');
-  audio.currentTime = 0;
-  audio.play();
-  }
-});
 
